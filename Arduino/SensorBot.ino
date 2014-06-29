613f4f247b131c3074714ee25b1ceae724c6baf4
Carmine <carmine.ferraro1@studenti.uniparthenope.it>
2014-06-30 00:30:03 +0200

    Update SensorBot.ino

--- Arduino/SensorBot.ino
+++ Arduino/SensorBot.ino
@@ -12,7 +12,7 @@
 //  GNU General Public License for more details.
 //
 //  You should have received a copy of the GNU General Public License
-//  along with Webdome.  If not, see <http://www.gnu.org/licenses/>.
+//  along with Sensorbot.  If not, see <http://www.gnu.org/licenses/>.
 
 int waterSensor = A0; 
 int tempSensor = A1;
